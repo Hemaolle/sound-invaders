ba248e15b0bc300e3dff32a7090593f5ab88f4f5
Oskari Leppäaho <oskari.leppaaho@gmail.com>
2013-10-12 10:38:13 +0300

    Started work on cannon script.

---
+++ The Game/Assets/Scripts/Cannon.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cannon : MonoBehaviour {
+	
+	public KeyCode activationKey;
+	
+	private Vector3 originalScale;
+	private bool activated = false;
+	private float activeSizeMultiplier = 1.2f;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(activationKey)) {
+			foreach (Cannon cannon in FindObjectOfType<Cannon>())
+				
+			
+			activated = true;
+			transform.localScale = originalScale * 1.2f;
+		}			
+	}
+	
+	public void InActivate() {
+		activated = false;
+		transform.localScale = originalScale;
+	}
+}
