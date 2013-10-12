047c0b9e517840eaaa4da805fc70d5d4b463d3a4
Oskari Leppäaho <oskari.leppaaho@gmail.com>
2013-10-12 18:54:24 +0300

    Game can be started after testing the cannons.

---
+++ The Game/Assets/Scripts/StartPlaying.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartPlaying : MonoBehaviour {
+	
+	private EnemySpawner enemySpawner;
+	
+	// Use this for initialization
+	void Start () {
+		enemySpawner = FindObjectOfType(typeof(EnemySpawner)) as EnemySpawner;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1) || Input.GetMouseButtonDown(2)) {
+			Debug.Log("Clicked");
+			enemySpawner.StartSpawning();
+			Destroy(gameObject);	
+		}
+	}
+}
