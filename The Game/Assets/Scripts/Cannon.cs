0d727f7c571a3e7816b427c1143c1ed2202c1b6e
Oskari Leppäaho <oskari.leppaaho@gmail.com>
2013-10-12 13:00:58 +0300

    Created some cannon materials and a NoteTuple class

--- The Game/Assets/Scripts/Cannon.cs
+++ The Game/Assets/Scripts/Cannon.cs
@@ -6,6 +6,7 @@ public class Cannon : MonoBehaviour {
 	public KeyCode activationKey;
 	public Note note1;
 	public Note note2;
+	public NoteTuple noteTuple;
 	
 	private Vector3 originalScale;
 	private bool activated = false;
@@ -13,7 +14,9 @@ public class Cannon : MonoBehaviour {
 	private LineRenderer laser;
 	private float laserShowTime = 0.3f;
 	
+	private static float activateCooldown = 0.1f;	
 	private static int lastLaserActivator;
+	private static float lastActivateTime;
 	
 	// Use this for initialization
 	void Start () {
@@ -24,7 +27,7 @@ public class Cannon : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKey(activationKey)) {
+		if (Input.GetKey(activationKey) && !activated) {// && Time.timeSinceLevelLoad - lastActivateTime > activateCooldown) {
 			Activate();
 		}
 		
@@ -56,6 +59,7 @@ public class Cannon : MonoBehaviour {
 		Debug.Log("Cannon activated");
 		StaticAudioPlayer.PlayNote(0.5f, note1.GetStOffset());
 		StaticAudioPlayer.PlayNote(0.5f, note2.GetStOffset());
+		//lastActivateTime = Time.timeSinceLevelLoad;
 	}
 	
 	public void InActivate() {
