aebd4c5e048a26aeed19d581ed116d56c4852d66
Oskari Leppäaho <oskari.leppaaho@gmail.com>
2013-10-13 15:36:59 +0300

    -Cannon is randomized. -All the 7 levels.

---
+++ The Game/Assets/Scripts/RandomizeCannons.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomizeCannons : MonoBehaviour {
+	
+	public GameObject[] cannons;
+	public KeyCode[] keyCodes;
+	public GameObject[] cannonLocations;
+	
+	private ArrayList usedCannons;
+	
+	// Use this for initialization
+	void Awake () {
+		usedCannons = new ArrayList();
+		int newCannonIndex;
+		for (int i = 0; i < cannonLocations.Length; i++) {
+			do {
+				newCannonIndex = Random.Range(0, cannons.Length);
+			} while(usedCannons.Contains(newCannonIndex));
+			GameObject newCannon = Instantiate(cannons[newCannonIndex], cannonLocations[i].transform.position, Quaternion.identity) as GameObject;
+			Destroy(cannonLocations[i]);
+			usedCannons.Add(newCannonIndex);
+			newCannon.GetComponent<Cannon>().activationKey = keyCodes[i];
+		}
+	}
+}
